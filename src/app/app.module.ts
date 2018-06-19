d7de06ba96d671fd87728778db30a8d4679d9aa1
Prita Hasjim <phasjim@gmail.com>
2018-06-19 16:49:41 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+/* Angular Material */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    MatButtonModule,
+    MatCheckboxModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
